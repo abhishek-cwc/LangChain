e0529057f2775fb7cc64e994c87746080c0230a0
abhishek <abhi86.pathak@gmail.com>
2026-04-14 19:10:18 +0530

    master

--- 1_llm.py
+++ 1_llm.py
@@ -1,9 +1,28 @@
 from github_llm import GitHubLLM
 from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_groq import ChatGroq
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 #ChatOpenAI replace GitHubLLM with ChatOpenAI
-model = GitHubLLM(model="gpt-4o", temperature=1.5, max_completion_tokens=5)
+# model = GitHubLLM(model="gpt-4o", temperature=1.5, max_completion_tokens=5)
 
+# response = model.invoke("What is capital of india")
+
+# print(response)
+
+
+# Using Groq
+
+model = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.7
+)
 response = model.invoke("What is capital of india")
-
 print(response)
